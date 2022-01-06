24cdf0be3f7754be1c12b2ba036f35448fcddeda
lbmountain <miss.lb.mountain@gmail.com>
2022-01-06 19:14:41 +0100

    New argument y_axis_limits to modify y axis limits. Improved script.

--- R/licornetics.R
+++ R/licornetics.R
@@ -5,7 +5,8 @@
 #' @param type Determines y axis data. Can be "gsw" (absolute stomatal conductance), "relgsw" (relative stomatal conductance), "A" (CO2 assimilation) or "WUE" (water use efficiency).
 #' @param area_correction Li-COR chamber size divided by average of measured leaf areas. Default is set to 1.
 #' @param timestamps Optionally add vertical lines to the plot as timeline indicators (e.g. 'c(20, 40, 60)').
-#' @param observations Crop the range of observations (time) you want to show (e.g. 'c(16:70)').
+#' @param observations Crop the range of observations (time) you want to show (e.g. '16:70').
+#' @param y_axis_limits Change the y axis limits (e.g. 'c(0,2)' with 0 being the lower and 2 the upper limit).
 #' @param legend_title Change the title of the legend. Default is set to "Genotype".
 #' @param legend_labels Change the labels within the legend. Default is set to the input given by the 'identifier' argument.
 #' @param remove_outliers Optionally remove boxplot outliers (based on data obtained by calculating A/gsw).
@@ -17,6 +18,7 @@ licornetics<- function(identifier,
                        area_correction=1,
                        timestamps=NULL,
                        observations=NULL,
+                       y_axis_limits=NULL,
                        legend_title="Genotype",
                        legend_labels=identifier,
                        remove_outliers="no") {
@@ -70,7 +72,7 @@ licornetics<- function(identifier,
 
 
 
-  ##correct absolute gsw values by measured area
+  ##correct absolute gsw and A by measured leaf area
   if (area_correction!=1) {
     licorall$gsw <- licorall$gsw*area_correction
     licorall$A <- licorall$A*area_correction
@@ -96,182 +98,92 @@ licornetics<- function(identifier,
 
   if (type=="gsw") {
     ##plot observations against absolute stomatal conductance (gsw)
-    if (is_empty(timestamps)) {
-      ggplot(licorgeno, mapping=aes(x=obs, y=mean_gsw))+
-        geom_errorbar(mapping=aes(ymin=mean_gsw-se_abs, ymax=mean_gsw+se_abs, colour=genotype), alpha=0.5, show.legend = F)+
-        geom_point(aes(colour=genotype))+
-        #        geom_label(mapping=aes(x=30, y=max(mean_gsw)+0.02, label="cloudy"))+
-        #        geom_label(mapping=aes(x=50, y=max(mean_gsw)+0.02, label="sunny"))+
-        #        geom_label(mapping=aes(x=70, y=max(mean_gsw)+0.02, label="dark"))+
-        guides(colour=guide_legend(title=legend_title))+
-        scale_colour_manual(values=colourblindpalette,
-                            labels=legend_labels)+
-        theme_classic()+
-        theme(legend.position = "bottom",
-              legend.justification="left",
-              legend.box.margin = margin(c(-10)),
-              legend.background = element_rect(fill=NA))+
-        labs(x="Time [min]", y=expression(paste("Absolute g"[SW], " [mol*m"^-2, "*s"^-1, "]")))
-    }
-    else {
-      #create df for vertical lines
-      timeline<- data.frame(mark=timestamps)
+    #create df for vertical lines
+    timeline<- data.frame(mark=timestamps)
 
-      #plot
-      ggplot(licorgeno, mapping=aes(x=obs, y=mean_gsw))+
-        geom_errorbar(mapping=aes(ymin=mean_gsw-se_abs, ymax=mean_gsw+se_abs, colour=genotype), alpha=0.5, show.legend = F)+
-        geom_point(aes(colour=genotype))+
-        geom_vline(timeline, mapping=aes(xintercept=timestamps), linetype="dotted")+
-        #        geom_label(mapping=aes(x=30, y=max(mean_gsw)+0.02, label="cloudy"))+
-        #        geom_label(mapping=aes(x=50, y=max(mean_gsw)+0.02, label="sunny"))+
-        #        geom_label(mapping=aes(x=70, y=max(mean_gsw)+0.02, label="dark"))+
-        guides(colour=guide_legend(title=legend_title))+
-        scale_colour_manual(values=colourblindpalette,
-                            labels=legend_labels)+
-        theme_classic()+
-        theme(legend.position = "bottom",
-              legend.justification="left",
-              legend.box.margin = margin(c(-10)),
-              legend.background = element_rect(fill=NA))+
-        labs(x="Time [min]", y=expression(paste("Absolute g"[SW], " [mol*m"^-2, "*s"^-1, "]")))
-    }
+    #plot
+    ggplot(licorgeno, mapping=aes(x=obs, y=mean_gsw))+
+      geom_errorbar(mapping=aes(ymin=mean_gsw-se_abs, ymax=mean_gsw+se_abs, colour=genotype), alpha=0.5, show.legend = F)+
+      geom_point(aes(colour=genotype))+
+      geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
+      guides(colour=guide_legend(title=legend_title))+
+      scale_colour_manual(values=colourblindpalette,
+                          labels=legend_labels)+
+      scale_y_continuous(limits = y_axis_limits)+
+      theme_classic()+
+      theme(legend.position = "bottom",
+            legend.justification="left",
+            legend.box.margin = margin(c(-10)),
+            legend.background = element_rect(fill=NA))+
+      labs(x="Time [min]", y=expression(paste("Absolute g"[SW], " [mol*m"^-2, "*s"^-1, "]")))
   }
 
 
   else {
     if (type=="relgsw") {
-      if (is_empty(timestamps)) {
-        ##plot observations against relative stomatal conductance (gsw)
-        ggplot(licorgeno, mapping=aes(x=obs, y=mean_relgsw))+
-          geom_errorbar(mapping=aes(ymin=mean_relgsw - se_rel, ymax=mean_relgsw + se_rel, colour=genotype), alpha=0.5, show.legend = F)+
-          geom_point(aes(colour=genotype))+
-          #            geom_label(mapping=aes(x=30, y=1.1, label="cloudy"))+
-          #            geom_label(mapping=aes(x=50, y=1.1, label="sunny"))+
-          #            geom_label(mapping=aes(x=70, y=1.1, label="dark"))+
-          scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), labels = c(0, 25, 50, 75, 100))+
-          guides(colour=guide_legend(title=legend_title))+
-          scale_colour_manual(values=colourblindpalette,
-                              labels=legend_labels)+
-          theme_classic()+
-          theme(legend.position = "bottom",
-                legend.justification="left",
-                legend.box.margin = margin(c(-10)),
-                legend.background = element_rect(fill=NA))+
-          labs(x="Time [min]", y=expression(paste("Relative g"[SW], " [%]")))
-      }
+      ##create data frame for vertical lines / timestamps
+      timeline<- data.frame(mark=timestamps)
 
-      else {
-        ##create data frame for vertical lines / timestamps
-        timeline<- data.frame(mark=timestamps)
-
-        ##plot observations against relative stomatal conductance (gsw)
-        ggplot(licorgeno, mapping=aes(x=obs, y=mean_relgsw))+
-          geom_errorbar(mapping=aes(ymin=mean_relgsw - se_rel, ymax=mean_relgsw + se_rel, colour=genotype), alpha=0.5, show.legend = F)+
-          geom_point(aes(colour=genotype))+
-          geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
-          #            geom_label(mapping=aes(x=30, y=1.1, label="cloudy"))+
-          #            geom_label(mapping=aes(x=50, y=1.1, label="sunny"))+
-          #            geom_label(mapping=aes(x=70, y=1.1, label="dark"))+
-          scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), labels = c(0, 25, 50, 75, 100))+
-          guides(colour=guide_legend(title=legend_title))+
-          scale_colour_manual(values=colourblindpalette,
-                              labels=legend_labels)+
-          theme_classic()+
-          theme(legend.position = "bottom",
-                legend.justification="left",
-                legend.box.margin = margin(c(-10)),
-                legend.background = element_rect(fill=NA))+
-          labs(x="Time [min]", y=expression(paste("Relative g"[SW], " [%]")))
-      }
+      ##plot observations against relative stomatal conductance (gsw)
+      ggplot(licorgeno, mapping=aes(x=obs, y=mean_relgsw))+
+        geom_errorbar(mapping=aes(ymin=mean_relgsw - se_rel, ymax=mean_relgsw + se_rel, colour=genotype), alpha=0.5, show.legend = F)+
+        geom_point(aes(colour=genotype))+
+        geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
+        scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), labels = c(0, 25, 50, 75, 100))+
+        guides(colour=guide_legend(title=legend_title))+
+        scale_colour_manual(values=colourblindpalette,
+                            labels=legend_labels)+
+        theme_classic()+
+        theme(legend.position = "bottom",
+              legend.justification="left",
+              legend.box.margin = margin(c(-10)),
+              legend.background = element_rect(fill=NA))+
+        labs(x="Time [min]", y=expression(paste("Relative g"[SW], " [%]")))
     }
 
 
     else {
       if (type=="A") {
-        if (is_empty(timestamps)) {
-          ##plot observations against CO2 assimilation (A)
-          ggplot(licorgeno, mapping=aes(x=obs, y=mean_A))+
-            geom_errorbar(mapping=aes(ymin=mean_A - se_A, ymax=mean_A + se_A, colour=genotype), alpha=0.5, show.legend = F)+
-            geom_point(aes(colour=genotype))+
-            #                geom_label(mapping=aes(x=30, y=max(mean_A)+3, label="cloudy"))+
-            #                geom_label(mapping=aes(x=50, y=max(mean_A)+3, label="sunny"))+
-            #                geom_label(mapping=aes(x=70, y=max(mean_A)+3, label="dark"))+
-            guides(colour=guide_legend(title=legend_title))+
-            scale_colour_manual(values=colourblindpalette,
-                                labels=legend_labels)+
-            theme_classic()+
-            theme(legend.position = "bottom",
-                  legend.justification="left",
-                  legend.box.margin = margin(c(-10)),
-                  legend.background = element_rect(fill=NA))+
-            labs(x="Time [min]", y=expression(paste("CO"[2], " assimilation [mol*m"^-2, "*s"^-1, "]")))
-        }
+        timeline<- data.frame(mark=timestamps)
 
-        else{
-          timeline<- data.frame(mark=timestamps)
-
-          ggplot(licorgeno, mapping=aes(x=obs, y=mean_A))+
-            geom_errorbar(mapping=aes(ymin=mean_A - se_A, ymax=mean_A + se_A, colour=genotype), alpha=0.5, show.legend = F)+
-            geom_point(aes(colour=genotype))+
-            geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
-            #                geom_label(mapping=aes(x=30, y=max(mean_A)+3, label="cloudy"))+
-            #                geom_label(mapping=aes(x=50, y=max(mean_A)+3, label="sunny"))+
-            #                geom_label(mapping=aes(x=70, y=max(mean_A)+3, label="dark"))+
-            guides(colour=guide_legend(title=legend_title))+
-            scale_colour_manual(values=colourblindpalette,
-                                labels=legend_labels)+
-            theme_classic()+
-            theme(legend.position = "bottom",
-                  legend.justification="left",
-                  legend.box.margin = margin(c(-10)),
-                  legend.background = element_rect(fill=NA))+
-            labs(x="Time [min]", y=expression(paste("CO"[2], " assimilation [mol*m"^-2, "*s"^-1, "]")))
-        }
+        ggplot(licorgeno, mapping=aes(x=obs, y=mean_A))+
+          geom_errorbar(mapping=aes(ymin=mean_A - se_A, ymax=mean_A + se_A, colour=genotype), alpha=0.5, show.legend = F)+
+          geom_point(aes(colour=genotype))+
+          geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
+          guides(colour=guide_legend(title=legend_title))+
+          scale_colour_manual(values=colourblindpalette,
+                              labels=legend_labels)+
+          scale_y_continuous(limits = y_axis_limits)+
+          theme_classic()+
+          theme(legend.position = "bottom",
+                legend.justification="left",
+                legend.box.margin = margin(c(-10)),
+                legend.background = element_rect(fill=NA))+
+          labs(x="Time [min]", y=expression(paste("CO"[2], " assimilation [mol*m"^-2, "*s"^-1, "]")))
       }
 
 
       else {
         if (type=="WUE") {
           ##plot observations against water use efficiency (WUE)
-          if (is_empty(timestamps)) {
-            ggplot(licorgeno, mapping=aes(x=obs, y=mean_WUE))+
-              geom_errorbar(mapping=aes(ymin=mean_WUE-se_WUE, ymax=mean_WUE+se_WUE, colour=genotype), alpha=0.5, show.legend = F)+
-              geom_point(aes(colour=genotype))+
-              #                  geom_label(mapping=aes(x=30, y=max(mean_WUE)+20, label="cloudy"))+
-              #                  geom_label(mapping=aes(x=50, y=max(mean_WUE)+20, label="sunny"))+
-              #                  geom_label(mapping=aes(x=70, y=max(mean_WUE)+20, label="dark"))+
-              guides(colour=guide_legend(title=legend_title))+
-              scale_colour_manual(values=colourblindpalette,
-                                  labels=legend_labels)+
-              theme_classic()+
-              theme(legend.position = "bottom",
-                    legend.justification="left",
-                    legend.box.margin = margin(c(-10)),
-                    legend.background = element_rect(fill=NA))+
-              labs(x="Time [min]", y=expression(paste("WUE [mol(CO"[2], ") * mol(H"[2],"O)"^-1)))
-          }
-          else {
-            #create df for vertical lines
-            timeline<- data.frame(mark=timestamps)
+          #create df for vertical lines
+          timeline<- data.frame(mark=timestamps)
 
-            #plot
-            ggplot(licorgeno, mapping=aes(x=obs, y=mean_WUE))+
-              geom_errorbar(mapping=aes(ymin=mean_WUE-se_WUE, ymax=mean_WUE+se_WUE, colour=genotype), alpha=0.5, show.legend = F)+
-              geom_point(aes(colour=genotype))+
-              geom_vline(timeline, mapping=aes(xintercept=timestamps), linetype="dotted")+
-              #                  geom_label(mapping=aes(x=30, y=min(mean_WUE)-20, label="cloudy"))+
-              #                  geom_label(mapping=aes(x=50, y=min(mean_WUE)-20, label="sunny"))+
-              #                  geom_label(mapping=aes(x=70, y=min(mean_WUE)-20, label="dark"))+
-              guides(colour=guide_legend(title=legend_title))+
-              scale_colour_manual(values=colourblindpalette,
-                                  labels=legend_labels)+
-              theme_classic()+
-              theme(legend.position = "bottom",
-                    legend.justification="left",
-                    legend.box.margin = margin(c(-10)),
-                    legend.background = element_rect(fill=NA))+
-              labs(x="Time [min]", y=expression(paste("WUE [mol(CO"[2], ") * mol(H"[2],"O)"^-1, "]")))
-          }
+          #plot
+          ggplot(licorgeno, mapping=aes(x=obs, y=mean_WUE))+
+            geom_errorbar(mapping=aes(ymin=mean_WUE-se_WUE, ymax=mean_WUE+se_WUE, colour=genotype), alpha=0.5, show.legend = F)+
+            geom_point(aes(colour=genotype))+
+            geom_vline(timeline, mapping=aes(xintercept=mark), linetype="dotted")+
+            guides(colour=guide_legend(title=legend_title))+
+            scale_colour_manual(values=colourblindpalette,
+                                labels=legend_labels)+
+            scale_y_continuous(limits = y_axis_limits)+
+            theme_classic()+
+            theme(legend.position = "bottom",
+                  legend.justification="left",
+                  legend.box.margin = margin(c(-10)),
+                  legend.background = element_rect(fill=NA))+
+            labs(x="Time [min]", y=expression(paste("WUE [mol(CO"[2], ") * mol(H"[2],"O)"^-1, "]")))
         }
       }
     }
